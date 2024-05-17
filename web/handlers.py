0991a8677c95233dea287e0f4cee4588bb22cbd7
Tim Engle <timothy.englew@gmail.com>
2024-05-17 13:59:16 -0700

    create index handler

--- web/handlers.py
+++ web/handlers.py
@@ -1,7 +1,18 @@
 from aiohttp import web
+import aiohttp
+import asyncio
 import aiohttp_jinja2
 
 
-@aiohttp_jinja2.template("home.html")
+@aiohttp_jinja2.template("leaderboard.html")
 async def index(request):
-    return {}
+    page = 1
+    count = 10
+    leaderboard_data = None
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"https://api.wordcraft.gg/api/top-xp?count={count}&page={page}"
+        ) as response:
+            leaderboard_data = await response.json()
+            print(leaderboard_data)
+    return {"leaderboard_data": leaderboard_data, "page": 1}
